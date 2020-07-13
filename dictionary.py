a8fe7e76997a621086fe40d82daa6377bd4e237a
xuwudawei <xuwudawei@outlook.com>
2020-07-14 04:39:01 +0530

    Improved in searching performance

--- dictionary.py
+++ dictionary.py
@@ -1,25 +1,35 @@
 import json
+from difflib import get_close_matches
 
 data=json.load(open("data.json"))
  
 def translate(search_word):
-    try:
-        if (search_word in data or search_word.title() in data or search_word.upper() in data or search_word.lower() in data):
-            i=1
-            print(f"\nMeaning of {search_word}:")
-            for line in data[search_word]:
-                print('\n'+str(i)+". "+line)
-                i+=1
-        else:
+    if (search_word in data):
+        i=1
+        print(f"\nMeaning of {search_word}:")
+        for line in data[search_word]:
+            print('\n'+str(i)+". "+line)
+            i+=1
+    elif (len(get_close_matches(search_word,data.keys()))>0):
+        print('\nDid you mean "%s" instead'%get_close_matches(search_word,data.keys())[0])
+        decide=input('Press y for YES and n for NO: ').lower()
+        if (decide=='y'):
+            k=1
+            print(f"\nMeaning of {get_close_matches(search_word,data.keys())[0]}:")
+            for line in data[get_close_matches(search_word,data.keys())[0]]:
+                print('\n'+str(k)+". "+line)
+                k+=1
+        elif (decide=='n'):
             print("\nSorry we do not have this word in our dictionary")
-    except:
+    else:
         print("\nSorry we do not have this word in our dictionary")
+
 
         
 
 flag='Y'
 while(flag=='Y'):
-    search_word=input("Enter the word to be searched: ")
+    search_word=input("Enter the word to be searched: ").lower()
     translate(search_word)
     opt=input("\nDo you want to continue to search:  Enter Y/N :").upper()
     if (opt=='Y'):
